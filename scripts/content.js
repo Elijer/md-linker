9e8dc24e505df13a1aff92494c3b8d85fa665805
Elijah Kennedy <elijahclimbs@gmail.com>
2023-05-05 16:35:33 -0500

    styled to show indicator upon copy

--- scripts/content.js
+++ scripts/content.js
@@ -1,14 +1,33 @@
-let product = ''
+
+// ----------------------------------------------------------------
+// # Functions
+
+const initiateMarkdownCopy = (text) => {
+    const absContainer = document.createElement("div")
+    const copySplash = document.createElement("div");
+    copySplash.setAttribute("id", "copy-splash")
+    copySplash.setAttribute("class", "hideMe")
+    navigator.clipboard.writeText(text)
+    copySplash.innerHTML = `markdown link copied!`
+    document.body.appendChild(copySplash)
+}
+
+// ----------------------------------------------------------------
+// # Listeners
+
+// initiateMarkdownCopy("HELLO")
+
+let selectedText = ''
 
 document.addEventListener("mouseup", (event) => {
     const selection = window.getSelection().toString()
-    product = `[${selection}](${window.location.href})`
+    selectedText = `[${selection}](${window.location.href})`
 });
 
 document.addEventListener('keydown', (event) => {
     if (event.key === "." && event.shiftKey === true && event.metaKey === true){
-        if (product.length > 1){
-            navigator.clipboard.writeText(product)
+        if (selectedText.length > 1){
+            initiateMarkdownCopy(selectedText)
         }
     }
 }, false);
